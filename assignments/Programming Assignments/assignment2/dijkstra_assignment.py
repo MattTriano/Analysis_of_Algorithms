561c2cf4895f117c8919ee018e89b1ea056d463b
Matt Triano <matt.triano@gmail.com>
2017-05-23 07:27:50 -0500

    figure out how to implement the iterator

--- assignments/Programming Assignments/assignment2/dijkstra_assignment.py
+++ assignments/Programming Assignments/assignment2/dijkstra_assignment.py
@@ -1,4 +1,5 @@
 import sys
+from heapq import heapify, heappop
 
 
 # vertex.pi: predecessor vertex
@@ -8,27 +9,34 @@ class Vertex:
         self.pi = None
         self.path_dist = sys.maxsize  # an approximation for infinity
         self.adj = {}
+        self.explored = False
 
     def get_id(self):
         return self.id
 
-    def add_adjacent(self, adjacent, weight=0):
-        self.adj[adjacent] = weight
+    def get_path_dist(self):
+        return self.path_dist
+
+    def set_path_dist(self, dist):
+        self.path_dist = dist
+
+    def get_prev(self):
+        return self.pi
 
     def set_prev(self, prev):
         self.pi = prev
 
-    def get_dist(self):
-        return self.path_dist
-
-    def get_prev(self):
-        return self.pi
+    def set_explored(self):
+        self.explored = True
 
     def get_adjacent_vertices(self):
         return self.adj.keys()
 
     def get_adjacent_weight(self, adj_node):
         return self.adj[adj_node]
+
+    def add_adjacent(self, adjacent, edge_weight=0):
+        self.adj[adjacent] = edge_weight
 
     def __repr__(self):
         return 'Vertex ' + str(self.id)
@@ -58,6 +66,9 @@ class Graph:
     def __init__(self):
         self.vertices = {}
         self.num_vertices = 0
+
+    def __init__(self):
+        return iter(self.vertices)
 
     def get_vertices(self):
         return self.vertices
@@ -114,39 +125,26 @@ def make_graph(filename):
     return network
 
 
-# def graph_printer(graph):
-#     for node_id in graph.get_vertices():
-#         node = graph.get_vertex(node_id)
-#         adj = []
-#         pretty_adj = []
-#         for adj_node in node.get_adjacent_vertices():
-#             adj.append(str(adj_node) + ' (dist = ' + str(node.get_adjacent_weight(adj_node)) + ')')
-#
-#         num_adj = len(adj)
-#         if num_adj > 0:
-#             pretty_adj.append(' is adjacent to ')
-#             for i in range(num_adj):
-#                 if i == 0:
-#                     pretty_adj.append(adj[i])
-#                 elif i == num_adj - 1:
-#                     pretty_adj.append(', and ' + adj[i])
-#                 else:
-#                     pretty_adj.append(', ' + adj[i])
-#             pretty_adj_string = ''.join(pretty_adj)
-#         else:
-#             pretty_adj_string = ' is not adjacent to any nodes.'
-#         print(str(node) + pretty_adj_string)
-
-
 def graph_printer(graph):
     for node_id in graph.get_vertices():
         node = graph.get_vertex(node_id)
         node.adj_print()
 
 
+def shortest_path_dijkstras(graph_filename, start_node, end_node):
+    graph = make_graph(graph_filename)
+    graph.get_vertex(start_node).set_path_dist(0)
+    unexplored_node_heap = []
+    for node in graph:
+        unexplored_node_heap.append((node, node.get_path_dist()))
+    heapify(unexplored_node_heap)
+    print('hi')
+
+
 def main():
-    graph_data1 = make_graph('Case1.txt')
-    graph_printer(graph_data1)
+    # graph_data1 = make_graph('Case1.txt')
+    # graph_printer(graph_data1)
+    shortest_path_dijkstras('Case1.txt', 'A', 'B')
     # graph_data_printer(graph_data1)
     print('hi')
 
